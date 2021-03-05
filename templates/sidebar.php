2b1599d75e62ba164d97fa26f2fe2abfa7377950
mailmrb94 <mailmrb94@gmail.com>
2021-03-06 01:08:41 +0530

    Ready for content

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -8,23 +8,31 @@
 										<li>
 											<span class="opener">EDUCATION</span>
 											<ul>
-												<li><a href="#">School</a></li>
-												<li><a href="#">Pre University</a></li>
-												<li><a href="#">Under Grad</a></li>
-												<li><a href="#">Post Grad</a></li>
+												<li><a href="education.php#mrb_kg">School</a></li>
+												<li><a href="education.php#mrb_pu">Pre University</a></li>
+												<li><a href="education.php#mrb_ug">Under Grad</a></li>
+												<li><a href="education.php#mrb_pg">Post Grad</a></li>
 											</ul>
 										</li>									
 										<li>
 											<span class="opener">CARRER</span>
 											<ul>
-												<li><a href="#">Renumerated</a></li>
-												<li><a href="#">Non Renumerated</a></li>
-												<li><a href="#">Volunteering</a></li>
-												<li><a href="#">Certification</a></li>
+												<li><a href="carrer.php#mrb_renumerated">Renumerated</a></li>
+												<li><a href="carrer.php#mrb_nonrenumerated">Non Renumerated</a></li>
+												<li><a href="carrer.php#mrb_volunteer">Volunteering</a></li>
+												<li><a href="carrer.php#mrb_certification">Certification</a></li>
+											</ul>
+										</li>
+										<li>
+										<span class="opener">Civil Services</span>
+											<ul>
+												<li><a href="cs.php#mrb_prelims">PRELIMS</a></li>
+												<li><a href="cs.php#mrb_mains">MAINS</a></li>
+												<li><a href="cs.php#mrb_essays">ESSAYS</a></li>
+												<li><a href="cs.php#mrb_pt">PERSONALITY TEST</a></li>
 											</ul>
 										</li>
 										<li><a href="trophy.php">Accomplishments</a></li>
-										<li><a href="generic.html">Civil Services</a></li>
 										<li><a href="blog.php">BLOGS</a></li>								
 										<li><a href="elements.html">INTERESTS - a Bucket List</a></li>
 									</ul>
---
+++ trophy.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<html>
+	<head>
+		<title>MAHESH R B</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="MAHESH R B"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C42F137QZ0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C42F137QZ0');
+        </script>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<?php include("templates/header.php");?>
+
+							<!-- Content -->
+								<section>
+									<header class="main">
+										<h1>ACCOMPLISHMENTS</h1>
+									</header>
+
+									<span class="image main"><img src="images/pic11.jpg" alt="" /></span>
+									
+									<h2 id="mrb_kg">KINDER GARTEN</h2>
+									
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<h2>PRIMARY SCHOOL</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+
+									<h2>HIGH SCHOOL</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<hr class="major" />
+
+									<h2 id="mrb_pu">PRE UNIVERSITY</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<hr class="major" />
+									
+									<h2 id="mrb_ug">UNDER GRADUATION</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<hr class="major" />
+									
+									<h2 id="mrb_pg">POST GRADUATION</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<hr class="major" />
+								</section>
+									
+								<?php include("templates/mainfooter.php");?>
+								
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+				<div id="sidebar">
+						<div class="inner">
+
+							<!-- Menu -->
+								 <?php include("templates/sidebar.php");?>
+								 <?php include("templates/getintouch.php");?>
+								
+							<!--Footer>
+								<footer id="footer">
+									<p class="copyright">&copy; MAHESH R B | PROUD INDIAN </p>
+								</footer> 
+							-->
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
