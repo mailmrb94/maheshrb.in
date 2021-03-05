bcade2cf21f765525c8ce2152588e1d7191c360a
mailmrb94 <mailmrb94@gmail.com>
2021-03-05 18:06:01 +0530

    nav change

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 						<div class="inner">
 
 							<!-- Header -->
-									<?php include("/templates/header.php");?>
+									<?php include("templates/header.php");?>
 
 							<!-- Banner -->
 								<section id="banner">
@@ -55,7 +55,7 @@
 									</span>
 								</section>
 								
-								<?php include("/templates/mainfooter.php");?>
+								<?php include("templates/mainfooter.php");?>
 						</div>
 					</div>
 
@@ -65,7 +65,7 @@
 
 							<!-- Menu --> 
 								 <?php include("sidebar.php");?>
-								 <?php include("/templates/getintouch.php");?>
+								 <?php include("../templates/getintouch.php");?>
 			
 							<!--Footer>
 								<footer id="footer">
